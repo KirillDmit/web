c2474a320bf9cf002d3c351e232e6bff52a83a57
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-23 21:01:34 +0500

    Update flask db.py

--- flask db.py
+++ flask db.py
@@ -10,7 +10,7 @@ def cv_index():
     cvs = get_cv()
     res = ""
     for i, cv in enumerate(cvs):
-        res += f"<h1>{i+1})</h1>"
+        res += f"<h1>{i + 1})</h1>"
         res += f"<p>Желаемая зарплата: {cv['salary']}.</p>"
         res += f"<p>Образование: {cv['educationType']}.</p>"
 
@@ -20,6 +20,13 @@ def cv_index():
 @app.route("/dashboard")
 def dashboard():
     return render_template('d1.html')
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('d2.html',
+                           cvs=get_cv(),
+                           )
 
 
 def dict_factory(cursor, row):
@@ -39,4 +46,3 @@ def get_cv():
 
 
 app.run()
-
